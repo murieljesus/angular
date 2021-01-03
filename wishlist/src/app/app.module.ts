1ce8a3e78e938d43fc78c224f1b72d1f33448f73
Jesus <jmuriel72@gmail.com>
2021-01-03 02:56:25 -0300

    'add-route'

--- wishlist/src/app/app.module.ts
+++ wishlist/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelComponent } from './travel/travel.component';
 import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+
+const routes: Routes =[
+  {path: '',redirectTo:'home', pathMatch:'full'},
+  {path:'home', component: ListComponent},
+  {path:'destino', component: DetailComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TravelComponent,
-    ListComponent
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
